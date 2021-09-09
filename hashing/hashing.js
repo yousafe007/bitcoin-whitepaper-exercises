615921195278e2374b2edaf294e430f8ff420ab2
Yousuf Ejazi <yousafe007@icloud.com>
2021-09-09 22:31:43 +0200

    A few methods implemented in the hashing.js file

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -28,8 +28,31 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	createBlock(line)
+}
+
+// Create a function called createBlock()
+// `index`
+// `prevHash`
+// `data`
+// `timestamp`
+// `hash`
+
+function createBlock(_data) {
+	let block = {
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+		data: _data,
+		timestamp: Date.now()
+	}
+	block.hash = blockHash(block)
+	Blockchain.blocks.push(block)
+	console.log(block)
+	return block
+}
+
+
 
 // console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
@@ -37,7 +60,12 @@ Blockchain.blocks.push({
 // **********************************
 
 function blockHash(bl) {
+	
+	// let block = JSON.stringify(bl)
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		// (`index`, `prevHash`, `data`, and `timestamp`)
+		`${bl.index};${bl.prevHash};${bl.data};${bl.timestamp};`
+		// block
 	).digest("hex");
 }
